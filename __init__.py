16f7875739a67c1edfd8cea366524920b4e374c1
natcebron <104511703+natcebron@users.noreply.github.com>
2022-06-21 13:27:51 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import os                      #+Deployment
+import inspect                 #+Deployment
+#importing all the necessary libraries
+import pandas as pd
+import numpy as np                     
+import matplotlib.pyplot as plt
+import os
+import random
+import seaborn as sns
+from PIL import Image, ImageStat,ImageOps
+import matplotlib.image as mpimg
+import streamlit as st
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2
+import numpy as np
+import pickle
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import models,utils
+import pandas as pd
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img,img_to_array
+from tensorflow.python.keras import utils
+import keras
+import matplotlib.cm as cm
+import streamlit.components.v1 as components
+
+def app():
+    def Gradcam(url):
+        img = keras.preprocessing.image.load_img(url, target_size = img_size) 
+        array = keras.preprocessing.image.img_to_array(img) 
+        array = np.expand_dims(array, axis = 0)
+        model = model_builder(weights = "imagenet")
+        model.layers[-1].activation = None
+        preds = model.predict(array) 
+        heatmap = make_gradcam_heatmap(array, model, last_conv_layer_name)
+        img = keras.preprocessing.image.load_img(url)
+        img = keras.preprocessing.image.img_to_array(img)
+        heatmap = np.uint8(255 * heatmap)
+        jet = cm.get_cmap("jet")
+        jet_colors = jet(np.arange(256))[:, :3]
+        jet_heatmap = jet_colors[heatmap]
+        jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+        jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+        jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+        superimposed_img = jet_heatmap * 1 + img
+        superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+        plt.axis('off')
+        rouge, vert, bleu = superimposed_img.split()
+        image_array = np.array(rouge,dtype='float64')
+        cv2.imwrite('data/images/gradcam.png',image_array)
+        return rouge
+
+    def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index = None):
+        grad_model = tf.keras.models.Model([model.inputs], [model.get_layer(last_conv_layer_name).output, model.output])
+
+        with tf.GradientTape() as tape:
+            last_conv_layer_output, preds = grad_model(img_array)
+            if pred_index is None:
+                pred_index = tf.argmax(preds[0])
+            class_channel = preds[:, pred_index]
+
+        grads = tape.gradient(class_channel, last_conv_layer_output)
+        pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+        last_conv_layer_output = last_conv_layer_output[0]
+        heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+        heatmap = tf.squeeze(heatmap)
+        heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+        return heatmap.numpy()  
+
+    model_builder = keras.applications.xception.Xception
+    img_size = (299, 299)
+    preprocess_input = keras.applications.xception.preprocess_input
+    decode_predictions = keras.applications.xception.decode_predictions
+    last_conv_layer_name = "block14_sepconv2_act"
+
+    imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
+
+    imageUrls = [
+        "https://images.unsplash.com/photo-1610016302534-6f67f1c968d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1075&q=80",
+        "https://images.unsplash.com/photo-1516550893923-42d28e5677af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80",
+        "https://images.unsplash.com/photo-1541343672885-9be56236302a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+        "https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+        "https://images.unsplash.com/photo-1528728329032-2972f65dfb3f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+        "https://images.unsplash.com/photo-1557744813-846c28d0d0db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1118&q=80",
+        "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+        "https://images.unsplash.com/photo-1595867818082-083862f3d630?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+        "https://images.unsplash.com/photo-1622214366189-72b19cc61597?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+        "https://images.unsplash.com/photo-1558180077-09f158c76707?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80",
+        "https://images.unsplash.com/photo-1520106212299-d99c443e4568?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+        "https://images.unsplash.com/photo-1534430480872-3498386e7856?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+        "https://images.unsplash.com/photo-1571317084911-8899d61cc464?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+        "https://images.unsplash.com/photo-1624704765325-fd4868c9702e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80",
+    ]
+    selectedImageUrl = imageCarouselComponent(imageUrls=imageUrls, height=200)
+
+    if selectedImageUrl is not None:
+        fig1 = plt.figure(figsize=(12, 12))
+        image = plt.imread(selectedImageUrl,format='fname')
+        cv2.imwrite('data/images/teest.png',image)
+        plt.imshow(image, cmap='gray')
+        plt.axis('off')
+
+        col1, mid,col2 = st.columns([3,3,3])
+
+        with col1:
+            st.header("Upload picture")
+            st.pyplot(fig1,use_column_width=True)
+        with mid:
+                fig2 = plt.figure(figsize=(12, 12))
+                st.header("Gradcam before correction")
+                plt.imshow(Gradcam('data/images/teest.png'))
+                st.pyplot(fig2)
+
+if __name__ == "__main__":
+    app()
