917047a3e908ba6ce5d6c972aabf0c1261dc91a9
natcebron <104511703+natcebron@users.noreply.github.com>
2022-06-22 08:55:59 +0200

    Update group.py

--- group.py
+++ group.py
@@ -24,14 +24,14 @@ def app():
     local_css(os.path.join(currentdir, "style.css"))
     #Préparation de la page
     st.markdown(""" <style> .font {font-size:20px ; font-family: 'Arial'; color: #FFFFFF;} </style> """, unsafe_allow_html=True)
-    st.markdown("# GROUP COMPARISON BY PICTURE CHARACTERISTICS")
+    st.markdown("# COMPARAISON DES GROUPES EN FONCTION DES CARACTÉRISTIQUES DE L'IMAGE")
     st.markdown('<p class="font"> Before the development of a deep learning model, a comparison of the different sets of images was carried out. To do this, a study of the characteristics of the images was performed). </p>', unsafe_allow_html=True)
     sns.set_theme(style="white", palette=None)
 
 ####################
-# PIXEL DISTRIBUTION 
+# DISTRIBUTION DES PIXELS 
 ####################
-    st.markdown('## Pixel distribution')
+    st.markdown('## DISTRIBUTION DES PIXELS')
 
 # retrieving data from file.
 
@@ -47,7 +47,7 @@ def app():
     load_original_arr_COVID = loaded_arr_COVID.reshape(loaded_arr_COVID.shape[0], loaded_arr_COVID.shape[1] // 3, 3)
     load_original_arr_NC = loaded_arr_NC.reshape(loaded_arr_NC.shape[0], loaded_arr_NC.shape[1] // 3, 3)
 
-    st.markdown('<p class="font"> The first step was to compare the distribution of pixels between the different image sets. This distribution study was carried out on an "average image" created from all the images in a set. </p>', unsafe_allow_html=True)
+    st.markdown("<p class='font'> La première étape a consisté à comparer la distribution des pixels entre les différents ensembles d'images. Cette étude de distribution a été réalisée sur une "image moyenne" créée à partir de toutes les images d'un ensemble. </p>", unsafe_allow_html=True)
 
     fig = plt.figure(figsize=(20, 7))
     plt.rcParams.update({'text.color': "white",'axes.labelcolor': "white",'text.color':'white','xtick.color':'white','ytick.color':'white','axes.facecolor':'#0e1117','axes.edgecolor':'#0e1117'})
@@ -59,21 +59,21 @@ def app():
     sns.kdeplot(arr_COVID,color='red',label='COVID')# Can show all four figures at once by calling plt.show() here, outside the loop.
     sns.kdeplot(arr_NC,color='green',label='NC')# Can show all four figures at once by calling plt.show() here, outside the loop.
     sns.kdeplot(arr_Normal,color='blue',label='Normal')# Can show all four figures at once by calling plt.show() here, outside the loop.
-    plt.title("Pixel intensity distribution",fontsize = 20);
-    plt.xlabel('Pixel intensity');
-    plt.ylabel('Density');
+    plt.title("Distribution de l'intensité du pixel",fontsize = 20);
+    plt.xlabel('Intensité de pixels');
+    plt.ylabel('Densité');
     plt.legend();
     plt.show()
 
     st.pyplot(fig)
-    st.markdown('<p class="font"> The pixel intensity distribution is very different between the different sets. Indeed, the images of the Non-COVID group show a symmetrical distribution with a peak at 140 while the Normal group has a peak at 120. For the COVID group, the distribution does not have a peak. This difference in distribution may have an impact and will therefore be taken into account during the image pre-processing stages.', unsafe_allow_html=True)
+    st.markdown("<p class='font'> La distribution de l'intensité des pixels est très différente entre les différents groupes. En effet, les images du groupe Non-COVID montrent une distribution symétrique avec un pic à 140 alors que le groupe Normal a un pic à 120. Pour le groupe COVID, la distribution ne présente pas de pic. Cette différence de distribution peut avoir un impact et sera donc prise en compte lors des étapes de prétraitement des images.", unsafe_allow_html=True)
 
 #############################################
 # PIXEL METRICS : MEAN AND STANDARD DEVIATION
 #############################################
 
-    st.markdown('## Pixel metrics (mean and std)')
-    st.markdown('<p class="font">To go further, a more detailed study of the pixels was carried out, including a determination of the pixel mean and pixel standard deviation metrics. These values were determined on each image of each set.  </p>', unsafe_allow_html=True)
+    st.markdown('## Métrique des pixels (moyenne et écart-type)')
+    st.markdown("<p class='font'>Pour aller plus loin, une étude plus détaillée des pixels a été réalisée, incluant une détermination des métriques de la moyenne des pixels et de l'écart-type des pixels. Ces valeurs ont été déterminées sur chaque image de chaque ensemble.  </p>", unsafe_allow_html=True)
 
     df = pd.read_csv(os.path.join(currentdir, 'data/Mean and std analysis.csv'))
 
@@ -95,14 +95,14 @@ def app():
     plt.text(122,0.0215,'120',color='b', fontsize=14)
     plt.text(150,0.0215,'145',color='r', fontsize=14)
 
-    plt.title("Mean",fontsize = 20);
-    plt.xlabel('Pixel intensity');
-    plt.ylabel('Density');
+    plt.title("Moyenne",fontsize = 20);
+    plt.xlabel('Intensité de pixel');
+    plt.ylabel('Densité');
     plt.legend();
     plt.show()
 
     st.pyplot(fig2)
-    st.markdown('<p class="font"> For all sets, the distribution of pixel mean values are symmetrical with the presence of a peak. This peak has a higher value for the COVID group with a value of 140 compared to the Normal and Non-COVID groups with values of 120 and 115 respectively. The hypothesis we can make is that the images in the COVID set have a different brightness, contrast or saturation compared to the other two sets but this remains to be confirmed. </p>', unsafe_allow_html=True)
+    st.markdown("<p class='font'> Pour tous les ensembles, la distribution des valeurs moyennes des pixels est symétrique avec la présence d'un pic. Ce pic a une valeur plus élevée pour le groupe COVID avec une valeur de 140 par rapport aux groupes Normal et Non-COVID avec des valeurs de 120 et 115 respectivement. L'hypothèse que nous pouvons émettre est que les images du groupe COVID ont une luminosité, un contraste ou une saturation différents par rapport aux deux autres groupes, mais cela reste à confirmer. </p>', unsafe_allow_html=True)
 
     fig3 = plt.figure(figsize=(20, 7))
     plt.rcParams.update({'text.color': "white",'axes.labelcolor': "white",'text.color':'white','xtick.color':'white','ytick.color':'white','axes.facecolor':'#0e1117','axes.edgecolor':'#0e1117'})
@@ -117,16 +117,16 @@ def app():
     sns.set_palette( my_colors )
   
     sns.boxplot(x=df['std'],y=df['group'], data=df)
-    plt.title("Standard deviation",fontsize = 20)
+    plt.title("Ecart-type",fontsize = 20)
     st.pyplot(fig3)
-    st.markdown('<p class="font"> The study of the standard deviation pixel variable was carried out. This metric was represented as a boxplot. The value of standard deviation seems to be lower in the COVID group with a mean value of 55 compared to the Normal group which has a value of 63. The Non-COVID group falls between these two sets with a value of 58. The images in the COVID set would therefore have a lower pixel variability.  </p>', unsafe_allow_html=True)
+    st.markdown("<p class='font'> L'étude de la variable pixel de l'écart-type a été réalisée. Cette métrique a été représentée sous forme de boxplot. La valeur de l'écart-type semble être plus faible dans le groupe COVID avec une valeur moyenne de 55 par rapport au groupe Normal qui a une valeur de 63. Le groupe Non-COVID se situe entre ces deux ensembles avec une valeur de 58. Les images du groupe COVID auraient donc une variabilité des pixels plus faible.   </p>", unsafe_allow_html=True)
 
 ##################
 # BRIGHTNESS STUDY
 ##################
 
-    st.markdown('## Brightness study')
-    st.markdown('<p class="font">The study of the luminosity between the different sets was carried out. This metric was measured on each of the images and a representation in the form of a density curve was made.  </p>', unsafe_allow_html=True)
+    st.markdown('## Étude de la luminosité')
+    st.markdown("<p class='font'>L'étude de la luminosité entre les différents ensembles a été réalisée. Cette métrique a été mesurée sur chacune des images et une représentation sous forme de courbe de densité a été réalisée.  </p>", unsafe_allow_html=True)
 
     df5 = pd.read_csv(os.path.join(currentdir, 'data/Brightness.csv'))
     fig4 = plt.figure(figsize=(20, 7))
@@ -135,20 +135,20 @@ def app():
     sns.kdeplot(df5['COVID'],color='red',label='COVID');# Can show all four figures at once by calling plt.show() here, outside the loop.
     sns.kdeplot(df5['NC'],color='green',label='NC');# Can show all four figures at once by calling plt.show() here, outside the loop.
     sns.kdeplot(df5['Normal'],color='blue',label='Normal');# Can show all four figures at once by calling plt.show() here, outside the loop.
-    plt.title("Brightness",fontsize = 20)
-    plt.xlabel('Brightness value');
+    plt.title("Luminosité",fontsize = 20)
+    plt.xlabel('Valeur de la luminosité');
 
     plt.legend();
 
     st.pyplot(fig4)
-    st.markdown('<p class="font">The density curves of the 3 sets are similar with a larger peak for the Non-COVID group. In addition, some images from the normal group show very high brightness values (200-250) compared to the peaks.  </p>', unsafe_allow_html=True)
+    st.markdown("<p class='font'>Les courbes de densité des 3 ensembles sont similaires avec un pic plus important pour le groupe Non-COVID. En outre, certaines images du groupe normal présentent des valeurs de luminosité très élevées (200-250) par rapport aux pics.  </p>", unsafe_allow_html=True)
 
 
 ################
 # CONTRAST STUDY
 ################
-    st.markdown('## Contrast study')
-    st.markdown('<p class="font"> The contrast study was also carried out. This was determined for each of the images by comparing the maximum and minimum pixel values for each of the images and then represented as a density curve.</p>', unsafe_allow_html=True)
+    st.markdown('## Étude du contraste')
+    st.markdown("<p class='font'> L'étude du contraste a également été réalisée. Celui-ci a été déterminé pour chacune des images en comparant les valeurs maximales et minimales des pixels pour chacune des images, puis représenté sous forme de courbe de densité.</p>", unsafe_allow_html=True)
 
     df6 = pd.read_csv(os.path.join(currentdir, 'data/Contrast.csv'))
     fig5 = plt.figure(figsize=(20, 7))
@@ -163,19 +163,19 @@ def app():
     sns.kdeplot(df6_NCOVID['contrast'],color='green',label='Non-COVID')# Can show all four figures at once by calling plt.show() here, outside the loop.
     sns.kdeplot(df6_Normal['contrast'],color='blue',label='Normal')# Can show all four figures at once by calling plt.show() here, outside the loop.
 
-    plt.title("Contrast",fontsize = 20);
-    plt.xlabel('Contrast value');
-    plt.ylabel('Density');
+    plt.title("Contraste",fontsize = 20);
+    plt.xlabel('Valeur du contraste');
+    plt.ylabel('Densité');
     plt.legend();
     plt.show()
     st.pyplot(fig5)
-    st.markdown('<p class="font">Les courbes de densité des 3 ensembles sont similaires avec un pic plus important pour le groupe Normal. </p>', unsafe_allow_html=True)
+    st.markdown("<p class='font'>Les courbes de densité des 3 ensembles sont similaires avec un pic plus important pour le groupe Normal. </p>', unsafe_allow_html=True)
 
 ########################
 # OUTLIERS DETERMINATION
 ########################
-    st.markdown('## Outliers determination')
-    st.markdown('<p class="font"> An outlier study was carried out. For this purpose the interquartile range (IQR) method was used on the previously determined standard deviation values. The results are presented in the form of a scatter plot according to the standard deviation and the mean of the pixels. If a value is considered to be an outlier then the size of the point on the scatter plot will be larger. </p>', unsafe_allow_html=True)
+    st.markdown('## Détermination des valeurs aberrantes')
+    st.markdown("<p class='font'> Une étude des valeurs aberrantes a été réalisée. Pour ce faire, la méthode de l'écart interquartile (IQR) a été utilisée sur les valeurs d'écart-type déterminées précédemment. Les résultats sont présentés sous la forme d'un nuage de points en fonction de l'écart-type et de la moyenne des pixels. Si une valeur est considérée comme aberrante, la taille du point sur le nuage de points sera plus grande. </p>", unsafe_allow_html=True)
 
     df6 = pd.read_csv(os.path.join(currentdir, 'data/outliers.csv'))
     Q1 = df6['O_stddev'].quantile(0.25)
@@ -189,12 +189,12 @@ def app():
     df6['outliers'] = df6['outliers'].replace((True, False), (10, 1))
 
     ax = sns.scatterplot(data=df6, x="O_mean", y='O_stddev', hue = 'group',size='outliers',alpha=0.8,palette=['green','red','blue']);
-    plt.title('Representation of the images as a function of the standard deviation and the mean',fontsize = 20);
-    plt.xlabel('Mean');
-    plt.ylabel('Standard deviation');
+    plt.title("Représentation des images en fonction de l'écart-type et de la moyenne",fontsize = 20);
+    plt.xlabel('Moyenne');
+    plt.ylabel('Ecart-type');
 
     st.pyplot(fig6)
-    st.markdown('<p class="font">The COVID group has a large number of outliers (340) compared to the Normal and Non-COVID sets which have 4 and 12 outliers respectively. The presence of these outliers may have an impact on the performance of the model and should be taken into account.  </p>', unsafe_allow_html=True)
+    st.markdown("<p class='font'>Le groupe COVID présente un grand nombre d'aberrations (340) par rapport aux ensembles Normal et Non-COVID qui présentent respectivement 4 et 12 aberrations. La présence de ces valeurs aberrantes peut avoir un impact sur la performance du modèle et doit être prise en compte.  </p>', unsafe_allow_html=True)
 
     st.markdown('## Forme des poumons ??')
 
