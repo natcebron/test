20a360af413afd930a5cfbb07bbb1079ad219d18
natcebron <104511703+natcebron@users.noreply.github.com>
2022-06-22 08:36:05 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,7 +30,7 @@ import matplotlib.cm as cm
 import streamlit.components.v1 as components
 import plotly.express as px
 def app():
-    st.markdown("# PICTURE INTERACTIVE ANALYSIS V1")
+    st.markdown("# ANALYSE DES IMAGES INTERACTIVES V1")
     predictor_model = load_model(os.path.join('Model_masks.hdf5'))
 
     def Gradcam(url):
