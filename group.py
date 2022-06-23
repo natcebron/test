d7fa3d3af2045d897c6eff2f4bf85ed841ec0d2a
natcebron <104511703+natcebron@users.noreply.github.com>
2022-06-23 11:57:20 +0200

    Update group.py

--- group.py
+++ group.py
@@ -125,8 +125,6 @@ def app():
     st.markdown('## Forme des poumons')
     image = Image.open(os.path.join(currentdir, 'data/forme.png'))
    
-    st.image(image, width=600)
-
-    st.markdown("<p class='font'> Les images présentes dans notre jeu de données sont toutes de dimension 256 pixels en hauteur et 256 pixels en largeur. </p>", unsafe_allow_html=True)
-
-    return None
+    col1, mid, col2 = st.columns([2,2,2])
+    with mid:
+        st.image(image, width=512)
