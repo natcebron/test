cffad27e6a051e905a76fd54ebcaa973485ec2b5
natcebron <104511703+natcebron@users.noreply.github.com>
2022-06-28 17:23:09 +0200

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -24,7 +24,6 @@ def app():
     st.markdown("<p class='font'> Le scanner et la radiographie du thorax, qui sont des outils d'imagerie de routine pour le diagnostic de la pneumonie, ont également été utilisés pour la détection des cas de COVID. Ils sont rapides et relativement faciles à réaliser en complément de l'examen clinique, sans être un test de détection virale à proprement parler. L'utilisation de ces technologies d'imagerie a conduit au développement de méthodes d'intelligence artificielle pour la détection automatique de virus à partir d'images pulmonaires.  </p>", unsafe_allow_html=True)
     st.markdown("<p class='font'> De nombreuses études sur la reconnaissance de rayons X basées sur l'apprentissage profond ont été réalisées. Dans une revue publiée en 2021 (Serena Low et al, 2021), 52 études publiées basées sur l'étude de radiographies ou de tomodensitogrammes entre 2019 et 2021 ont été résumées avec l'algorithme utilisé et les résultats obtenus. </p>", unsafe_allow_html=True)
     st.markdown("<p class='font'> Dans notre étude, nous avons utilisé le dataset COVID-QU-Ex présent sur Kaggle.          </p>", unsafe_allow_html=True)
-    st.markdown("<p class='font'> Lien jeu de données <a href="https://www.kaggle.com/datasets/anasmohammedtahir/covidqu">COVID-QU-Ex Dataset</a>.</p>", unsafe_allow_html=True)
 
  
           
