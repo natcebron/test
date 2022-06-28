487f71d20d0e039f796cd32cdcfa350374f458fd
natcebron <104511703+natcebron@users.noreply.github.com>
2022-06-28 17:21:40 +0200

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -20,16 +20,11 @@ def app():
     #Préparation de la page
     st.markdown(""" <style> .font {font-size:16px ; font-family: 'Arial'; color: #FFFFFF;} </style> """, unsafe_allow_html=True)
     st.markdown("# DEVELOPPEMENT D'UN MODELE DE DEEP LEARNING POUR LA CLASSIFICATION DE RADIOGRAPHIES DANS UN CONTEXTE COVID")
-    st.markdown("<p class='font'> Le COVID-19 est une maladie infectieuse causée par le virus du SRAS-Cov-2. Ce virus affecte l'homme et a un tropisme pour les poumons. 
- </p>", unsafe_allow_html=True)
-    st.markdown("<p class='font'> Le scanner et la radiographie du thorax, qui sont des outils d'imagerie de routine pour le diagnostic de la pneumonie, ont également été utilisés pour la détection des cas de COVID. Ils sont rapides et relativement faciles à réaliser en complément de l'examen clinique, sans être un test de détection virale à proprement parler. L'utilisation de ces technologies d'imagerie a conduit au développement de méthodes d'intelligence artificielle pour la détection automatique de virus à partir d'images pulmonaires.  
- </p>", unsafe_allow_html=True)
-    st.markdown("<p class='font'> De nombreuses études sur la reconnaissance de rayons X basées sur l'apprentissage profond ont été réalisées. Dans une revue publiée en 2021 (Serena Low et al, 2021), 52 études publiées basées sur l'étude de radiographies ou de tomodensitogrammes entre 2019 et 2021 ont été résumées avec l'algorithme utilisé et les résultats obtenus. 
- </p>", unsafe_allow_html=True)
-    st.markdown("<p class='font'> Dans notre étude, nous avons utilisé le dataset COVID-QU-Ex présent sur Kaggle.          
- </p>", unsafe_allow_html=True)
-    st.markdown("<p class='font'> Lien jeu de données <a href="https://www.kaggle.com/datasets/anasmohammedtahir/covidqu">COVID-QU-Ex Dataset</a>.
- </p>", unsafe_allow_html=True)
+    st.markdown("<p class='font'> Le COVID-19 est une maladie infectieuse causée par le virus du SRAS-Cov-2. Ce virus affecte l'homme et a un tropisme pour les poumons. </p>", unsafe_allow_html=True)
+    st.markdown("<p class='font'> Le scanner et la radiographie du thorax, qui sont des outils d'imagerie de routine pour le diagnostic de la pneumonie, ont également été utilisés pour la détection des cas de COVID. Ils sont rapides et relativement faciles à réaliser en complément de l'examen clinique, sans être un test de détection virale à proprement parler. L'utilisation de ces technologies d'imagerie a conduit au développement de méthodes d'intelligence artificielle pour la détection automatique de virus à partir d'images pulmonaires.  </p>", unsafe_allow_html=True)
+    st.markdown("<p class='font'> De nombreuses études sur la reconnaissance de rayons X basées sur l'apprentissage profond ont été réalisées. Dans une revue publiée en 2021 (Serena Low et al, 2021), 52 études publiées basées sur l'étude de radiographies ou de tomodensitogrammes entre 2019 et 2021 ont été résumées avec l'algorithme utilisé et les résultats obtenus. </p>", unsafe_allow_html=True)
+    st.markdown("<p class='font'> Dans notre étude, nous avons utilisé le dataset COVID-QU-Ex présent sur Kaggle.          </p>", unsafe_allow_html=True)
+    st.markdown("<p class='font'> Lien jeu de données <a href="https://www.kaggle.com/datasets/anasmohammedtahir/covidqu">COVID-QU-Ex Dataset</a>.</p>", unsafe_allow_html=True)
 
  
           
