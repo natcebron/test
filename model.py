df435f9f0f93c311315edcff2cfdca01b75346db
natcebron <104511703+natcebron@users.noreply.github.com>
2022-06-29 09:52:22 +0200

    Update model.py

--- model.py
+++ model.py
@@ -42,7 +42,17 @@ def app():
     st.markdown("## SEGMENTATION DES IMAGES")
     st.markdown("<p class='font'> La segmentation des images a été réalisé à l'aide d'un modèle UNET. Ce modèle a été entrainé sur un tiers du dataset. </p>", unsafe_allow_html=True)
     st.markdown("<p class='font'> Accuracy : 0.99 </p>", unsafe_allow_html=True)
+    col1, col2, col3 = st.columns(3)
 
+    with col1:
+        image = Image.open(os.path.join(currentdir, 'data/covid_1.png'))
+        st.image(image)
+
+    with col2:
+        image = Image.open(os.path.join(currentdir, 'data/mask_covid_1.png'))
+        st.image(image)
+    with col3:
+        st.write(' ')
     
     st.markdown("## VISUALISATION")
     st.markdown("<p class='font'> Pour tenter d'éliminer les biais, diverses méthodes de prétraitement ont été testées. Nous avons testé l'application de masques en utilisant un modèle UNET, un filtre gaussien : Une méthode de flou de l'image pour réduire le bruit de l'image, ajuster le gamma : Une méthode de correction pour contrôler la luminosité, l'égalisation d'histogramme adaptative limitée par le contraste (CLAHE), une technique pour modifier l'image en améliorant le contraste. La dernière méthode est un mélange de CLAHE et d'un filtre de transformation. </p>", unsafe_allow_html=True)
